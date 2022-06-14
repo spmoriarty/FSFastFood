b8af232a3982ddceb9e00546602babc3d374e37c
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-14 16:56:45 -0700

    added drinkdropdown

--- src/InstructionList.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function InstructionList() {
-  return (
-    <div>InstructionList</div>
-  );
-}
---
+++ src/NameInput.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NameInput() {
+  return (
+    <div>NameInput</div>
+  );
+}
