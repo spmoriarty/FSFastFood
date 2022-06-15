fe2a9e5707efc8c6d18210a95d874050623895d8
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-15 15:55:00 -0700

    Order Name fx

--- src/NameInput.js
+++ src/NameInput.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-export default function NameInput() {
-  return (
-    <div>NameInput</div>
-  );
+export default function NameInput({ setName }) {
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  return <div className='order-name'>
+    Order For:
+    <input onChange={handleChange} />
+  </div>;
+  
 }
