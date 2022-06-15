6f4920b964548458a087e54005958de200101493
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-14 17:00:23 -0700

    added breakfast foods

--- src/FoodDropdown.js
+++ src/FoodDropdown.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function FoodDropdown() {
-  return (
-    <div>FoodDropdown</div>
-  );
+export default function FoodDropdown({} setFoodId }) {
+  function handleChange(e){
+    setFoodId(e.target.value);
+  }
+  return <select onChange={handleChange}>
+<option value='1'>French Toast</option>
+<option value='2'>Eggs Benedict</option>
+<option value='3'>Chef's Breakfast</option>
+
+  </select>;
+  
 }
