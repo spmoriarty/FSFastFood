acb26cdb451ccf8247697fdfc288d57ea8396347
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-14 17:04:50 -0700

    added sides options

--- src/SideItemDropdown.js
+++ src/SideItemDropdown.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-export default function SideItemDropdown() {
-  return (
-    <div>SideItemDropdown</div>
-  );
+export default function SideItemDropdown({ setSideItemId }) {
+  function handleChange(e) {
+    setSideItemId(e.target.value);
+  }
+
+  return <select onChange={handleChange}>
+    <option value='1'>Bacon</option>
+    <option value='2'>Hash Potatos</option>
+    <option value='3'>Fresh Fruit</option>
+  </select>;
 }
