c5d4fe068c052906078343a8def8ac91a217ac43
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-15 17:24:55 -0700

    fixed side item display

--- src/SideItemDropdown.js
+++ src/SideItemDropdown.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function SideItemDropdown({ setSideItemId }) {
+export default function SideItemDropdown({ setSideId }) {
   function handleChange(e) {
-    setSideItemId(e.target.value);
+    setSideId(e.target.value);
   }
 
   return <select onChange={handleChange}>
